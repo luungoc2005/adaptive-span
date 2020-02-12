2edffc2a9077af6d78a244557ee10ac6ba7b954a
luungoc2005 <luungoc2005@gmail.com>
2020-02-13 00:02:21 +0800

    train a bigger model

--- experiments/reddit.sh
+++ experiments/reddit.sh
@@ -6,24 +6,25 @@ args="
 --data data/reddit \
 --tokenizer_vocab data/reddit/vocab/en-vocab.json \
 --tokenizer_merges data/reddit/vocab/en-merges.txt \
---nlayers 8 \
---hid-sz 256 \
---inner-hid-sz 1024 \
---nheads 4 \
---attn-span 1024 \
---block-sz 256 \
---batch-sz 80 \
+--nlayers 12 \
+--hid-sz 512 \
+--inner-hid-sz 2048 \
+--nheads 8 \
+--attn-span 4096 \
+--block-sz 512 \
+--batch-sz 64 \
 --lr 3e-4 \
 --momentum 0 \
---dropout 0 \
+--dropout 0.15 \
 --optim adamw \
---lr-warmup 8000 \
+--lr-warmup 32000 \
 --grad-clip 1.0 \
---niter 150 \
+--niter 250 \
 --nbatches 1000 \
 --adapt-span \
---adapt-span-loss 0.000002 \
---adapt-span-cache
+--adapt-span-loss 0.0000005 \
+--adapt-span-cache \
+--batch-split 2 \
 --checkpoint checkpoints/reddit_small.pt
 "
 
