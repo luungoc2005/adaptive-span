9b8a1a9a5db377d89420033fd839de062b49f667
luungoc2005 <luungoc2005@gmail.com>
2020-02-20 00:06:06 +0800

    minimal example

--- experiments/reddit_small.sh
+++ experiments/reddit_small.sh
@@ -6,25 +6,21 @@ args="
 --data data/reddit \
 --tokenizer_vocab data/reddit/vocab/en-vocab.json \
 --tokenizer_merges data/reddit/vocab/en-merges.txt \
---nlayers 8 \
---hid-sz 256 \
---inner-hid-sz 1024 \
+--nlayers 2 \
+--hid-sz 128 \
+--inner-hid-sz 512 \
 --nheads 4 \
---attn-span 4096 \
---block-sz 256 \
+--attn-span 512 \
+--block-sz 512 \
 --batch-sz 64 \
 --optim radam \
 --lr 3e-4 \
 --momentum 0 \
 --dropout .1 \
 --lr-warmup 8000 \
---grad-clip 0.03 \
+--grad-clip 0.5 \
 --niter 150 \
 --nbatches 1000 \
---adapt-span \
---adapt-span-loss 0.000001 \
---adapt-span-cache
---batch-split 2 \
 --checkpoint checkpoints/reddit_small.pt
 "
 
